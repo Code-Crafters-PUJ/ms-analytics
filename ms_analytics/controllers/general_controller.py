edf96d7418e9f2af03fba5ebea54989ceb0b48a5
Santicm23 <Santicastrom@outlook.com>
2024-04-26 08:46:10 -0500

    general feature finished

--- ms_analytics/controllers/general_controller.py
+++ ms_analytics/controllers/general_controller.py
@@ -1,3 +1,5 @@
+import json
+
 from ..config.constants.app import GeneralChartXOption, GeneralChartYOption
 from ..dtos.output import GeneralChartInfoDto
 
@@ -5,9 +7,12 @@ from ..dtos.output import GeneralChartInfoDto
 async def get_general_info(
     x_type: GeneralChartXOption, y_type: GeneralChartYOption
 ) -> GeneralChartInfoDto:
+    with open("ms_analytics/data/data.json") as f:
+        data = json.load(f)
+
     return GeneralChartInfoDto(
         x_label=x_type,
         y_label=y_type,
-        x=[1, 2, 3],
-        y=["product1", "product2", "product3"],
+        x=data["general"]["x"][x_type.value],
+        y=data["general"]["y"][y_type.value],
     )
--- ms_analytics/dtos/output/general_chart_info_dto.py
+++ ms_analytics/dtos/output/general_chart_info_dto.py
@@ -6,5 +6,5 @@ from ...config.constants.app import GeneralChartXOption, GeneralChartYOption
 class GeneralChartInfoDto(BaseModel):
     x_label: GeneralChartXOption
     y_label: GeneralChartYOption
-    x: list[int | float]  # total sales or income
-    y: list[int | str]  # product name or time period
+    x: list[int | str]  # most selled products or time period
+    y: list[int | float]  # total sales or income
