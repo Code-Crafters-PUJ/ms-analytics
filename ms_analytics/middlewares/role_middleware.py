1edd5d453b4eba6807a1f02573c35926916050c9
Santicm23 <Santicastrom@outlook.com>
2024-04-27 18:52:35 -0500

    restoring role validation

--- ms_analytics/middlewares/role_middleware.py
+++ ms_analytics/middlewares/role_middleware.py
@@ -8,16 +8,15 @@ from ..helpers.jwt import get_payload
 
 def validate_role(*roles: Role) -> Callable[[str | None], None]:
     def validate_role(token: str | None = Header(None)) -> None:
-        # if token is None:
-        #     raise HTTPException(status_code=401, detail="Token is required")
+        if token is None:
+            raise HTTPException(status_code=401, detail="Token is required")
 
-        # try:
-        #     payload = get_payload(token)
-        # except Exception:
-        #     raise HTTPException(status_code=401, detail="Invalid token")
+        try:
+            payload = get_payload(token)
+        except Exception:
+            raise HTTPException(status_code=401, detail="Invalid token")
 
-        # if payload["Role"] not in roles:
-        #     raise HTTPException(status_code=403, detail="Forbidden")
-        ...
+        if payload["Role"] not in roles:
+            raise HTTPException(status_code=403, detail="Forbidden")
 
     return validate_role
