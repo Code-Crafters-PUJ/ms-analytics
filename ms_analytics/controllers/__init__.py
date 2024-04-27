815c66cb5558ab8a13733f799721bddb38f332f0
Santicm23 <Santicastrom@outlook.com>
2024-04-27 18:38:39 -0500

    finishing sales routes

--- ms_analytics/controllers/__init__.py
+++ ms_analytics/controllers/__init__.py
@@ -5,6 +5,11 @@ from .inventory_controller import (
     provider_vs_stock_percentage,
 )
 from .payroll_controller import fortnight_vs_salary, month_vs_salary
+from .sales_controller import (
+    branch_vs_sales,
+    month_vs_income,
+    top_saled_products_vs_sales,
+)
 
 __all__ = [
     "category_vs_purchase_and_sales",
@@ -14,4 +19,7 @@ __all__ = [
     "provider_vs_stock_percentage",
     "fortnight_vs_salary",
     "month_vs_salary",
+    "branch_vs_sales",
+    "month_vs_income",
+    "top_saled_products_vs_sales",
 ]
--- ms_analytics/routers/sales_router.py
+++ ms_analytics/routers/sales_router.py
@@ -2,16 +2,39 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends
 
-from ms_analytics.config.constants.app import Role
+from ms_analytics.controllers.sales_controller import (
+    branch_vs_sales,
+    month_vs_income,
+    top_saled_products_vs_sales,
+)
+from ms_analytics.dtos.output.sales_dtos import (
+    BranchVsSalesDto,
+    MonthVsIncomeDto,
+    TopSaledProductsVsSalesDto,
+)
 
+from ..config.constants.app import Role
 from ..middlewares import validate_role
 
 router = APIRouter()
 
-@router.get("/info")
-async def get_info_sales(
+
+@router.get("/branch-vs-sales")
+async def get_branch_vs_sales(
     _: Annotated[None, Depends(validate_role(Role.ADMIN))],
-    x_type: None,
-    y_type: None,
-) -> None:
-    ...
+) -> BranchVsSalesDto:
+    return await branch_vs_sales()
+
+
+@router.get("/month-vs-income")
+async def get_month_vs_income(
+    _: Annotated[None, Depends(validate_role(Role.ADMIN))],
+) -> MonthVsIncomeDto:
+    return await month_vs_income()
+
+
+@router.get("/top-saled-products-vs-sales")
+async def get_top_saled_products_vs_sales(
+    _: Annotated[None, Depends(validate_role(Role.ADMIN))],
+) -> TopSaledProductsVsSalesDto:
+    return await top_saled_products_vs_sales()
