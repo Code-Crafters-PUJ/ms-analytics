840c28dc71ab6c797b931c0c17d2ab66fb0a3049
Santicm23 <santicastrom@outlook.com>
2024-05-11 18:56:19 -0500

    fixing ip issues

--- ms_analytics/server.py
+++ ms_analytics/server.py
@@ -17,7 +17,7 @@ load_metadata(app)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
